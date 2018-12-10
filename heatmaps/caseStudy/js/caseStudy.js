e730f9a2ecbfcd4dce88118aa0d0dfa46db68f52
Kristen Wenzel <kmwenzel@hotmail.com>
2018-12-10 12:34:03 -0600

    Update caseStudy.js

--- heatmaps/caseStudy/js/caseStudy.js
+++ heatmaps/caseStudy/js/caseStudy.js
@@ -33,7 +33,7 @@ map.on('load', function(){
     paint: {
       'fill-color': '#ffffff',
       'fill-outline-color': '#FF0000',
-      'fill-opacity': 0.8
+      'fill-opacity': 0.4
     }
   });
   map.addLayer({
@@ -49,7 +49,7 @@ map.on('load', function(){
     paint: {
       'fill-color': '#ffffff',
       'fill-outline-color': '#FF0000',
-      'fill-opacity': 0.8
+      'fill-opacity': 0.4
     }
   });
     map.addSource("clientPoint", {
